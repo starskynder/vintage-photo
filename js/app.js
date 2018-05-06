968820ae94adf7a9e305d917d76f3284db8d27eb
starskynder <witchywisher@gmail.com>
2018-05-06 19:19:18 +0200

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+let card = document.getElementsByClassName("vintage-team_card");
+console.log(card);
+
+Array.from(card).forEach(v => {
+    v.addEventListener("mouseover", changeCard);
+
+
+});
+Array.from(card).forEach(v => {
+    v.addEventListener("mouseout", initialCard);
+
+
+});
+
+function initialCard() {
+
+    this.style.backgroundColor = "#fff";
+    this.style.color = "#000";
+    this.children[2].style.visibility = "hidden";
+    this.children[0].style.marginTop = "100px";
+}
+
+function changeCard(e) {
+    console.log(this);
+    console.log(this.children);
+    this.style.backgroundColor = "rgb(55, 80, 128)";
+    this.style.color = "#fff";
+    this.children[0].style.marginTop = "-100px";
+    this.children[2].style.visibility = "visible";
+
+}
